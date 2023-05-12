f61f4e2190f4445957ee5bd11f0630fe6bfdc983
Gustavo Mayorga <redvioletoficial@gmail.com>
2023-05-12 10:58:52 -0600

    Creando componente selladora

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { SelladoraComponent } from './selladora/selladora.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SelladoraComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/selladora/selladora.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-selladora',
+  templateUrl: './selladora.component.html',
+  styleUrls: ['./selladora.component.scss']
+})
+export class SelladoraComponent {
+
+}
