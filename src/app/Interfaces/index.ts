82807ea9765b4ad6f761addbbbe678c21c7c8bbc
Gustavo Mayorga <redvioletoficial@gmail.com>
2023-05-12 12:07:16 -0600

    Creacion de la clase de graficas radeal

--- src/app/Interfaces/index.ts
+++ src/app/Interfaces/index.ts
@@ -1,5 +1,8 @@
 import { AbstractControl, FormGroup } from "@angular/forms";
+import { ApexChart, ApexFill, ApexNonAxisChartSeries, ApexPlotOptions, ApexStroke } from "ng-apexcharts";
 
+//-----------------------------------------
+//Interfaz utilizada en pagina de login para formulario y creacion de variables
 export interface login{
     username: string;
     password: string;
@@ -12,3 +15,15 @@ export interface loginForm extends FormGroup{
         password: AbstractControl<any>,
     }
 }
+
+//-----------------------------------------
+//Interfaz creada para las graficas radiales creadas con clase
+export interface GraficaRadial {
+    series: ApexNonAxisChartSeries,
+    chart: ApexChart,
+    labels: string[],
+    plotOptions: ApexPlotOptions,
+    fill: ApexFill,
+    stroke: ApexStroke
+}
+//-----------------------------------------
---
+++ src/app/pages/selladora/RadialGraph.ts
@@ -0,0 +1,115 @@
+import {
+    ApexNonAxisChartSeries,
+    ApexPlotOptions,
+    ApexChart,
+    ApexFill,
+    ApexStroke
+  } from "ng-apexcharts";
+
+export class RadialGraph{
+
+    public series: ApexNonAxisChartSeries = [75]
+
+    public chart: ApexChart = {
+      height: 350,
+      type: "radialBar",
+      toolbar: {
+        show: true
+      }
+    }
+  
+    public labels: string[] = ["OEE"]
+  
+    public plotOptions: ApexPlotOptions = {
+      radialBar: {
+        startAngle: -135,
+        endAngle: 225,
+        hollow: {
+          margin: 0,
+          size: "70%",
+          background: "#fff",
+          image: undefined,
+          position: "front",
+          dropShadow: {
+            enabled: true,
+            top: 3,
+            left: 0,
+            blur: 4,
+            opacity: 0.24
+          }
+        },
+        track: {
+          background: "#fff",
+          strokeWidth: "67%",
+          margin: 0, // margin is in pixels
+          dropShadow: {
+            enabled: true,
+            top: -3,
+            left: 0,
+            blur: 4,
+            opacity: 0.35
+          }
+        },
+  
+        dataLabels: {
+          show: true,
+          name: {
+            offsetY: -10,
+            show: true,
+            color: "#888",
+            fontSize: "17px"
+          },
+          value: {
+            formatter: function (val) {
+              return parseInt(val.toString(), 10).toString();
+            },
+            color: "#111",
+            fontSize: "36px",
+            show: true
+          }
+        }
+      }
+    }
+  
+    public fill: ApexFill = {
+      type: "gradient",
+      gradient: {
+        shade: "dark",
+        type: "horizontal",
+        shadeIntensity: 0.5,
+        gradientToColors: ["#ABE5A1"],
+        inverseColors: true,
+        opacityFrom: 1,
+        opacityTo: 1,
+        stops: [0, 100]
+      }
+    } 
+  
+    public stroke: ApexStroke = {
+      lineCap: "round"
+    }
+
+    
+    constructor(){
+    }
+    
+    //Define la etiqueta de titulo para que tiene la grafica
+    defineLabel(label: string){
+        this.labels = [label];
+    }
+
+    //Define el valor porcentual de la grafica
+    defineValue(value: number){
+       this.series = [value];
+    }
+
+    getParameters(){
+        const series = this.series;
+        const chart = this.chart;
+        const plotOptions = this.plotOptions;
+        const labels = this.labels;
+        const stroke = this.stroke;
+        const fill = this.fill;
+       return { series, chart, plotOptions, labels, stroke, fill }
+    }
+}
